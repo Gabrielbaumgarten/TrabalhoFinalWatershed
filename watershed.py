cf8443c63aaa977b1e2072bb6c79cb4d6772cbf7
Gabriel Baumgarten <gabrielbaumgarten@hotmail.com>
2022-06-15 16:52:45 -0300

    Pintando parede a partir de uma máscara de fundo e outra de frente

--- watershed.py
+++ watershed.py
@@ -59,46 +59,20 @@ def main ():
     # Apply watershed
     markers = cv2.watershed(img, markers)
 
-    markers[0] = [1] * markers.shape[1]
-    markers[markers.shape[0]-1] = [1] * markers.shape[1]
+    # markers[0] = [1] * markers.shape[1]
+    # markers[markers.shape[0]-1] = [1] * markers.shape[1]
 
-    for row in range(markers.shape[0]):
-        markers[row][0] = 1
-        markers[row][markers.shape[1]-1] = 1
+    # for row in range(markers.shape[0]):
+    #     markers[row][0] = 1
+    #     markers[row][markers.shape[1]-1] = 1
     
-    img[markers == -1] = [255, 0, 0]
-    
-    cImg = img.copy()
 
-    for marker in np.unique(markers):
-        # if the label is zero, we are examining the 'background'
-        # so simply ignore it
-        if marker == 0:
-            continue
-        # otherwise, allocate memory for the label region and draw
-        # it on the mask
-        mask = np.zeros(img.shape, dtype="uint8")
-        mask[markers == marker] = 255
-        cv2.imshow('mask', mask)
-        # detect contours in the mask and grab the largest one
-        cnts, h = cv2.findContours(cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        
-        cv2.drawContours(cImg, cnts, 0, (0, 255, 0), 1)
-        # cnts = imutils.grab_contours(cnts)
-        # c = max(cnts, key=cv2.contourArea)
-        # # draw a circle enclosing the object
-        # ((x, y), r) = cv2.minEnclosingCircle(c)
-        # cv2.circle(img, (int(x), int(y)), int(r), (0, 255, 0), 2)
-        # cv2.putText(img, "#{}".format(label), (int(x) - 10, int(y)),
-        #     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-
-    cv2.imshow('contornos', cImg)
     
     img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
 
     for row in range(img.shape[0]):
         for col in range(img.shape[1]):
-            if sure_fg[row][col]==0 :
+            if markers[row][col]==1 :
     #             # print('areas azul')
                 img_hls[row][col] = [80,(img_hls[row][col][1]),183.6]
     #             # img_hsl[row][col] = [322 ,98,img_hsl[row][col][2]]
@@ -107,7 +81,8 @@ def main ():
     cv2.imshow('changeColor', img_hls)
 
 
-    cv2.imshow('Camera', img)
+    img[markers == -1] = [255, 0, 0]
+    cv2.imshow('Contornos', img)
 
 
     cv2.waitKey ()
