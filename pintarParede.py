bcdc05b1f1a36d1adec848bedc63dbf90206de87
Gabriel Baumgarten <gabrielbaumgarten@hotmail.com>
2022-06-28 22:28:51 -0300

    Criado seletor de frente e fundo

--- pintarParede.py
+++ pintarParede.py
@@ -51,7 +51,8 @@ def draw_circle(event,x,y,flags,param):
         else:
             cv2.circle(img,(x,y),5,(0,0,255),-1)
 
-img = np.zeros((512,512,3), np.uint8)
+# img = np.zeros((512,512,3), np.uint8)
+img = cv2.imread('./quadros.jpg', cv2.IMREAD_COLOR)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',draw_circle)
 
--- watershed.py
+++ watershed.py
@@ -6,87 +6,163 @@ from skimage.feature import peak_local_max
 from skimage.segmentation import watershed
 from scipy import ndimage
 
+drawing = False # true if mouse is pressed
+mode = True # if True, draw rectangle. Press 'm' to toggle to curve
+ix,iy = -1,-1
+
 INPUT_IMAGE =  './quadros.jpg'
 
-def main ():
+def selectBackground(event,x,y,flags, param):
+    global ix,iy,drawing,mode
 
-    img = cv2.imread (INPUT_IMAGE, cv2.IMREAD_COLOR)
-    # img = cv2.resize(img, (floor(img.shape[0]/2), floor(img.shape[1]/2)))
-    # img = img.astype(np.float)/255
-    cv2.imshow('Original', img)
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
 
-    
-    # gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # gray_img = cv2.medianBlur(gray_img, 11)
-    # _, filtered = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    
-    # cv2.imshow('Threshold', filtered)
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            cv2.circle(sure_bg,(x,y),15,(0,0,0),-1)
+            cv2.circle(sure_bg_mask,(x,y),15,(0,0,0),-1)
+            # cv2.rectangle(sure_bg,(x,y),(x+10,y+10),(-1,-1,-1),-1)
+            # for row in range(10):
+            #     for col in range(10):
+            #         sure_bg[x+row][y+col] =[-1,-1,-1]
 
-    # # # Remove noise
-    kernel = np.ones((3, 3), np.uint8)
-    # opening = cv2.morphologyEx(filtered.copy(), cv2.MORPH_OPEN, kernel, iterations = 2)
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.circle(sure_bg,(x,y),15,(0,0,0),-1)
+        cv2.circle(sure_bg_mask,(x,y),15,(0,0,0),-1)
+        # cv2.rectangle(sure_bg,(x,y),(x+10,y+10),(-1,-1,-1),-1)
+        # for row in range(10):
+        #     for col in range(10):
+        #         sure_bg[x+row][y+col] =[-1, -1,-1]
 
-    # # cv2.imshow('Opening', opening)
+def selectForeground(event,x,y,flags, param):
+    global ix,iy,drawing,mode
 
-    # # # Find background area
-    # sure_bg = cv2.dilate(opening, kernel, iterations = 3)
-    sure_bg = cv2.imread ('./BG.png', cv2.IMREAD_GRAYSCALE)
-    cv2.imshow('Sure BG', sure_bg)
-    # cv2.imwrite ('BG.png', sure_bg)
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
 
-    # # Find foreground area
-    # dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-    # _, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
-    
-    sure_fg = cv2.erode(sure_bg, kernel, iterations = 7)
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            cv2.circle(sure_fg,(x,y),15,(255,255,255),-1)
+            cv2.circle(sure_fg_mask,(x,y),15,(255,255,255),-1)
 
-    cv2.imshow('Sure FG', sure_fg)
-
-    # # Find unknown region
-    sure_fg = np.uint8(sure_fg)
-    unknown = cv2.subtract(sure_bg, sure_fg)
-    
-    cv2.imshow('Unknown', unknown)
-
-    # sure_fg = cv2.imread ('./quadros_mascara.jpg', cv2.IMREAD_GRAYSCALE)
-    # cv2.imshow('Mask', sure_fg)
-    # Add marker labels
-    _, markers = cv2.connectedComponents(sure_fg)
-    # markers = cv2.findContours(sure_fg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    markers = markers + 1
-    markers[unknown == 255] = 0
-
-    # Apply watershed
-    markers = cv2.watershed(img, markers)
-
-    # markers[0] = [1] * markers.shape[1]
-    # markers[markers.shape[0]-1] = [1] * markers.shape[1]
-
-    # for row in range(markers.shape[0]):
-    #     markers[row][0] = 1
-    #     markers[row][markers.shape[1]-1] = 1
-    
-
-    
-    img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-
-    for row in range(img.shape[0]):
-        for col in range(img.shape[1]):
-            if markers[row][col]==1 :
-    #             # print('areas azul')
-                img_hls[row][col] = [80,(img_hls[row][col][1]),183.6]
-    #             # img_hsl[row][col] = [322 ,98,img_hsl[row][col][2]]
-
-    img_hls = cv2.cvtColor(img_hls, cv2.COLOR_HLS2BGR)
-    cv2.imshow('changeColor', img_hls)
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.circle(sure_fg,(x,y),15,(255,255,255),-1)
+        cv2.circle(sure_fg_mask,(x,y),15,(255,255,255),-1)
 
 
-    img[markers == -1] = [255, 0, 0]
-    cv2.imshow('Contornos', img)
+img = cv2.imread (INPUT_IMAGE, cv2.IMREAD_COLOR)
+# img = cv2.resize(img, (floor(img.shape[0]/2), floor(img.shape[1]/2)))
+# img = img.astype(np.float)/255
+cv2.imshow('Original', img)
+
+sure_bg = img.copy()
+sure_bg_mask = np.ones((img.shape[0], img.shape[1]), np.uint8)
+cv2.namedWindow('background')
+cv2.setMouseCallback('background',selectBackground)
+
+while(1):
+    cv2.imshow('background',sure_bg)
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('m'):
+        mode = not mode
+    elif k == 13 or k == 27:
+        break
+
+sure_bg_mask = np.where(sure_bg_mask != 0, 255, 0)
+sure_bg_mask = sure_bg_mask.astype(np.uint8)
+cv2.imshow('Sure BG', sure_bg_mask)
+
+sure_fg = img.copy()
+sure_fg_mask = np.zeros((img.shape[0], img.shape[1]), np.uint8)
+cv2.namedWindow('foreground')
+cv2.setMouseCallback('foreground',selectForeground)
+
+while(1):
+    cv2.imshow('foreground',sure_fg)
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('m'):
+        mode = not mode
+    elif k == 13 or k == 27:
+        break
+
+sure_fg_mask = np.where(sure_fg_mask != 255, 0, 255)
+sure_fg_mask = sure_fg_mask.astype(np.uint8)
+cv2.imshow('Sure FG', sure_fg_mask)
+
+# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# gray_img = cv2.medianBlur(gray_img, 11)
+# _, filtered = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+# cv2.imshow('Threshold', filtered)
+
+# # # Remove noise
+# kernel = np.ones((3, 3), np.uint8)
+# opening = cv2.morphologyEx(filtered.copy(), cv2.MORPH_OPEN, kernel, iterations = 2)
+
+# # cv2.imshow('Opening', opening)
+
+# # # Find background area
+# sure_bg = cv2.dilate(opening, kernel, iterations = 3)
+# sure_bg = cv2.imread ('./BG.png', cv2.IMREAD_GRAYSCALE)
+# cv2.imshow('Sure BG', sure_bg)
+# cv2.imwrite ('BG.png', sure_bg)
+
+# # Find foreground area
+# dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+# _, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
+
+# sure_fg = cv2.erode(sure_bg, kernel, iterations = 7)
+
+# cv2.imshow('Sure FG', sure_fg)
+
+# # Find unknown region
+# sure_fg = np.uint8(sure_fg)
+unknown = cv2.subtract(sure_bg_mask, sure_fg_mask)
+
+cv2.imshow('Unknown', unknown.astype(np.uint8))
+
+# sure_fg = cv2.imread ('./quadros_mascara.jpg', cv2.IMREAD_GRAYSCALE)
+# cv2.imshow('Mask', sure_fg)
+# Add marker labels
+_, markers = cv2.connectedComponents(sure_fg_mask)
+# markers = cv2.findContours(sure_fg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+markers = markers + 1
+markers[unknown == 255] = 0
+
+# Apply watershed
+markers = cv2.watershed(img, markers)
+
+# markers[0] = [1] * markers.shape[1]
+# markers[markers.shape[0]-1] = [1] * markers.shape[1]
+
+# for row in range(markers.shape[0]):
+#     markers[row][0] = 1
+#     markers[row][markers.shape[1]-1] = 1
 
 
-    cv2.waitKey ()
-    cv2.destroyAllWindows ()
 
-if __name__ == '__main__':
-    main ()
+img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+
+for row in range(img.shape[0]):
+    for col in range(img.shape[1]):
+        if markers[row][col]==1 :
+#             # print('areas azul')
+            img_hls[row][col] = [80,(img_hls[row][col][1]),183.6]
+#             # img_hsl[row][col] = [322 ,98,img_hsl[row][col][2]]
+
+img_hls = cv2.cvtColor(img_hls, cv2.COLOR_HLS2BGR)
+cv2.imshow('changeColor', img_hls)
+
+
+img[markers == -1] = [255, 0, 0]
+cv2.imshow('Contornos', img)
+
+
+cv2.waitKey ()
+cv2.destroyAllWindows ()
+
